cb22bf9d88dbe2ca8fe21526a4790c0934a34e9b
Faris <faris.faruk@catalyca.com>
2026-07-24 15:44:53 +0530

    Refactor cashAdvanceScreen to use probeElement

--- pages/cart/cash-advance-request-page.ts
+++ pages/cart/cash-advance-request-page.ts
@@ -1,5 +1,23 @@
 import logger from '@wdio/logger'
+
 const log = logger('CashAdvanceRequestPage')
+
+async function probeElement(
+  driver: WebdriverIO.Browser,
+  selector: string,
+  attempts = 25,
+  interval = 1000,
+): Promise<WebdriverIO.Element> {
+  for (let i = 0; i < attempts; i++) {
+    const els = await driver.$$(selector);
+    if (els.length > 0) {
+      const isDisplayed = await els[0].isDisplayed().catch(() => false);
+      if (isDisplayed) return els[0];
+    }
+    await driver.pause(interval);
+  }
+  throw new Error(`Element not found after ${attempts} attempts: ${selector}`);
+}
 
 export class CashAdvanceRequest {
   driver: WebdriverIO.Browser;
@@ -10,65 +28,85 @@ export class CashAdvanceRequest {
 
   async cashAdvanceScreen() {
     const driver = this.driver;
+
     await driver.pause(6000);
-    log.info(
-      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111               ",
-   );
-    const menuTab = await driver.$(
+
+    const menuTab = await probeElement(
+      driver,
       '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button',
+      30,
+      1000,
     );
-    await menuTab.waitForDisplayed({ timeout: 30000 });
     await menuTab.click();
-    log.info("menu tab clicked");
-
+    log.info('menu tab clicked');
     await driver.pause(5000);
-    const cashAdvanceTab = await driver.$(
+
+    const cashAdvanceTab = await probeElement(
+      driver,
       '//android.view.View[@content-desc="Cash Advance"]',
+      25,
+      1000,
     );
-    await cashAdvanceTab.waitForDisplayed({ timeout: 25000 });
     await cashAdvanceTab.click();
-    log.info("cash advance tab clicked");
+    log.info('cash advance tab clicked');
     await driver.pause(5000);
-    const firstCard = await driver.$(
-      "(//android.view.View[contains(@content-desc,'Submitted by')])[1]",
-    );
 
-    await firstCard.waitForDisplayed({ timeout: 25000 });
-    await firstCard.click();
-    const viewDetails = await driver.$(
-      "//android.view.View[@content-desc='View Detail']",
+    const firstCard = await probeElement(
+      driver,
+      "(//android.view.View[contains(@content-desc,'Submitted by')])[1]",
+      35,
+      1000,
     );
-    await viewDetails.waitForDisplayed({ timeout: 25000 });
-    log.info("request details displayed");
+    await firstCard.click();
+    log.info('first cash advance card clicked');
+
+    const viewDetails = await probeElement(
+      driver,
+      "//android.view.View[@content-desc='View Detail']",
+      25,
+      1000,
+    );
+    log.info('request details displayed');
     await viewDetails.click();
     await driver.pause(6000);
-    const viewEntryDetailsBackButton = await driver.$(
+
+    const viewEntryDetailsBackButton = await probeElement(
+      driver,
       "//android.widget.Button[@content-desc='Back']",
+      25,
+      1000,
     );
-    await viewEntryDetailsBackButton.waitForDisplayed({ timeout: 25000 });
-    log.info("back button displayed");
+    log.info('back button displayed');
     await viewEntryDetailsBackButton.click();
     await driver.pause(6000);
 
-    const auditViewButton = await driver.$(
+    const auditViewButton = await probeElement(
+      driver,
       'android=new UiSelector().className("android.widget.Button").instance(1)',
+      25,
+      1000,
     );
-    await auditViewButton.waitForDisplayed({ timeout: 25000 });
-    log.info("audit  details displayed");
+    log.info('audit details displayed');
     await auditViewButton.click();
     await driver.pause(6000);
-    const workFlowAuditButton = await driver.$(
+
+    const workFlowAuditButton = await probeElement(
+      driver,
       '//android.widget.Button[@content-desc="Workflow Audit"]',
+      25,
+      1000,
     );
-    await workFlowAuditButton.waitForDisplayed({ timeout: 25000 });
-    log.info("audit  details displayed");
+    log.info('workflow audit button displayed');
     await workFlowAuditButton.click();
     await driver.pause(6000);
-    const workflowAuditDoneButton = await driver.$(
+
+    const workflowAuditDoneButton = await probeElement(
+      driver,
       '//android.widget.Button[@content-desc="Done"]',
+      25,
+      1000,
     );
-    await workflowAuditDoneButton.waitForDisplayed({ timeout: 25000 });
-    log.info("done button displayed");
+    log.info('done button displayed');
     await workflowAuditDoneButton.click();
   }
 }
